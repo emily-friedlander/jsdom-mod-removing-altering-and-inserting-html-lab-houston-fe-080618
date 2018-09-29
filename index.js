0c30199c6fce21b1cd345d2faeaa0eb7c4f7c445
Emily Friedlander <emily@emilyfriedlander.com>
2018-09-29 18:10:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /* Enter the code to remove the main node element under this comment */
 // body.removeChild(body.querySelector('#main'));
+const testVar = "hi"
 document.querySelector("main#main").remove();
 /* Create your new element here and assign it to newHeader */
 // const newHeader = null;
