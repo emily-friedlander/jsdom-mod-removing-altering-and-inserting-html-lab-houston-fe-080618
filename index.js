c9fdbe1c4eba354cd683af4fc1a75b9a6e3cae7e
Emily Friedlander <emily@emilyfriedlander.com>
2018-09-29 18:07:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 /* Enter the code to remove the main node element under this comment */
-
+// body.removeChild(body.querySelector('#main'));
+main.remove();
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+// const newHeader = null;
+// document.body.appendChild(h1)
+// document.createElement(h1)
+var h = document.createElement ("H1") (id='victory');
+var t = document.createTextNode ("Emily is the champion");
+h.appendChild(t);
+document.body.appendChild(h);
