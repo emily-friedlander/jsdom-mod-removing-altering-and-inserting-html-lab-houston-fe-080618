67f319ba4665d970747cf8d000fde3c574658c86
Emily Friedlander <emily@emilyfriedlander.com>
2018-09-29 18:10:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ document.querySelector("main#main").remove();
 // const newHeader = null;
 // document.body.appendChild(h1)
 // document.createElement(h1)
-var h = document.createElement ("H1") (id='victory');
-var t = document.createTextNode ("Emily is the champion");
-h.appendChild(t);
-document.body.appendChild(h);
+// var h = document.createElement ("H1") (id='victory');
+// var t = document.createTextNode ("Emily is the champion");
+// h.appendChild(t);
+// document.body.appendChild(h);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerText = "Emily is the champion!";
+newHeader.className = "victory";
